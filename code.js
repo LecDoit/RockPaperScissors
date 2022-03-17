dc7d504babcd1763370476794a7579f4ba7fd312
pikimaru <pikajunglist@gmail.com>
2022-03-17 12:11:09 +0100

    New function 'play()' perform an actual one round of paper, rock scissors game and result in console.log message and points +1, 0, -1 which indicates if you won or lost.

--- code.js
+++ code.js
@@ -1,12 +1,30 @@
 function computerPlay(){
-    let words = ['rock', 'papper', 'scissors'];
+    let words = ['rock', 'paper', 'scissors'];
     let word = words[Math.floor((Math.random() * words.length))];
-    console.log(word)
-    return (word)
+    return word
 }
 
 function playerSelection(){
     var input = window.prompt('Choose: paper,rock or scissors.')
-    console.log(input)
     return input
 }
+
+function play(x,y){
+    userInput = x
+    computerInput = y
+    let start = 0
+    if (x === y)
+    { console.log("draw")}
+    else if ((x == 'rock' && y=='paper') || (x == 'paper' && y=='scissors') || (x == 'scissors' && y=='rock'))
+    {console.log("u loose!")
+    start --}
+    else if((x =='rock' && y =='scissors')||(x =='paper' && y =='rock')||(x =='scissors' && y =='paper'))
+    {console.log("u won badman!")
+    start++}
+    console.log(userInput,computerInput, start)
+    return ((userInput,computerInput, start))
+
+}
+
+play(playerSelection(),computerPlay())
+
