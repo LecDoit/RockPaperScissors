8f30c6628b4dc88ab72373d3205d8abfbd9f7425
pikimaru <pikajunglist@gmail.com>
2022-03-17 11:09:09 +0100

    Adding JS code with function that generates random number at pick ups the item from list.

---
+++ code.js
@@ -0,0 +1,10 @@
+function computerPlay(){
+    var words = ['rock', 'papper', 'scissors'];
+    var randomword = words[Math.floor(Math.random()) *  words.length];
+    let test = Math.floor(Math.random());
+    let test2 = words.length;
+    let test3 = words[Math.floor((Math.random() * words.length))];
+    
+    console.log(test3)
+
+}
